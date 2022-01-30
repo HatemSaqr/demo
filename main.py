81256d6d77fbcf416b81a2ffe713a275aeee0a97
hatim <sqrhatm064@gmail.com>
2022-01-30 22:55:29 +0300

    calculator

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from pickle import TRUE
+
+
 print("Calculator")
-num1 = float(input("enter first number:"))
-num2 = float(input("enter sacend number:"))
+
 
 def add(y,x):
     return y + x
@@ -8,6 +10,45 @@ def add(y,x):
 def mines (y,x):
     return y - x
 
-print(add(num1,num2))
-print(mines(num1,num2))
+def mult (y,x):
+    return y * x
+
+def g (y,x):
+    return y / x
+
+calculator = True
+
+while calculator:
+    
+    print("   ")
+    print("type +,-,*,/,exit")
+    choies = input("choise: ")
+    if choies == "exit":
+        calculator = False
+        break
+    
+    
+    print("   ")
+    
+    num1 = float(input("enter first number:"))
+    
+    print("   ")
+    
+    num2 = float(input("enter sacend number:"))
+    
+    
+    if choies == "+":
+        print(add(num1,num2))
+   
+    elif choies == "-":
+        print(mines(num1,num2))
+
+    elif choies == "*":
+        print(mult(num1,num2))
+
+    elif choies == "/":
+        print(g(num1,num2))
+
+    else:
+        print("error")
 
